1e1525b49fe4adf8b9b0ef255fdb1c57f14ef28c
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-06-24 23:41:56 -0700

    added a function of adding new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import { TodoType } from "./types";
 
-function App() {
+const todosData = [
+  { id: "1", title: "todo title 1" },
+  { id: "2", title: "todo title 2" },
+  { id: "3", title: "todo title 3" },
+  { id: "4", title: "todo title 4" },
+  { id: "5", title: "todo title 5" },
+  { id: "6", title: "todo title 6" },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState(todosData);
+  const handleDeleteTodo = (id: string) => {
+    const filterTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filterTodos);
+  };
+
+  const handleAddNewTodo = (newTodo: TodoType) => {
+    setTodos((prevState) => [...prevState, newTodo]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddTodo handleAddNewTodo={handleAddNewTodo} />
+      <Todos todos={todos} handleDeleteTodo={handleDeleteTodo} />
     </div>
   );
-}
+};
 
 export default App;
